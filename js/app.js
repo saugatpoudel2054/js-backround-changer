9b52b951bbd783a4b47bd89b03bb34177f887274
Saugat Poudel <saugatpoudel2054@gmail.com>
2021-06-26 19:18:25 +0545

    Completed project

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
+const colorArray = ['A', 'B', 'C', 'D', 'E', 'F', 0,1,2,3,4,5,6,7,8,9];
 
+const btn = document.getElementById("btn");
+
+btn.addEventListener('click', () => {
+    var color = generateRandomColor();
+    console.log(color);
+    document.body.style.backgroundColor = color;
+})
+
+const generateRandomColor = () => {
+    var color = '#';
+    for (let index = 0; index < 6; index++) {
+        var number = Math.trunc(Math.random() * colorArray.length);
+        color = color + colorArray[number];
+    }
+
+    return color;
+}
